64276c95792a95689e6ab92524d00df1068d0afe
Octane Silvio <ruslangibadullin70@gmail.com>
2022-06-24 21:53:45 +0500

    added fragment package with main Fragment, deleted Action bar, MainFragment on activity_main.xml

--- app/src/main/java/com/octaneocatane/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/octaneocatane/weatherforecast/MainActivity.kt
@@ -2,10 +2,15 @@ package com.octaneocatane.weatherforecast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.octaneocatane.weatherforecast.databinding.FragmentMainBinding
+import com.octaneocatane.weatherforecast.fragments.MainFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager
+            .beginTransaction().replace(R.id.placeHolder, MainFragment.newInstance())
+            .commit()
     }
 }
