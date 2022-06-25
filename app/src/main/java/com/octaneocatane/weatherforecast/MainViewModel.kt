aa1b470cd0f2ff3620992c59e8dfff6116042271
Octane Silvio <ruslangibadullin70@gmail.com>
2022-06-25 19:00:29 +0500

    added graddle dependecies, manifest permissions, MainViewModel class

---
+++ app/src/main/java/com/octaneocatane/weatherforecast/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.octaneocatane.weatherforecast
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    val liveDataCurrent = MutableLiveData<String>()
+    val liveDataList = MutableLiveData<String>()
+}
